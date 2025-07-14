90a63ab730dba0ded6efce349769fa57954ae4c0
rnsingh2025 <rnsingh.bjsps@gmail.com>
2025-07-14 13:36:53 +0000

    updated

--- modules/EC2/publicEC2/main.tf
+++ modules/EC2/publicEC2/main.tf
@@ -16,3 +16,40 @@ resource "aws_instance" "public_ec2" {
         Name = "${each.key}"
     }
 }
+# resource "aws_eip" "public_ip" {
+#   for_each = var.public_instances
+#   instance = aws_instance.public_ec2[each.key].id
+#   vpc      = true
+# }
+
+# resource "null_resource" "copy_pem" {
+#   depends_on = [aws_instance.public_ec2]
+#   triggers = {
+#     instance_id = aws_instance.public_ec2["master"].id
+#   }
+
+#   connection {
+#       type        = "ssh"
+#       user        = "ubuntu"
+#       private_key = file("${pathexpand("~")}/${var.pem_file_name}.pem")
+#       timeout     = "5m"
+#       host        = aws_eip.public_ip["master"].public_ip
+#     }
+
+
+#   provisioner "remote-exec" {
+#     inline = [
+#       "until (-f /var/lib/cloud/instance/boot-finished); do sleep 2; done"
+#     ]
+#     }
+#   provisioner "file" {
+#     source      = "${pathexpand("~")}/${var.pem_file_name}.pem"
+#     destination = "/home/ubuntu/${var.pem_file_name}.pem"
+#     }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "chmod 400 /home/ubuntu/${var.pem_file_name}.pem",
+#       "sudo chown ubuntu:ubuntu /home/ubuntu/${var.pem_file_name}.pem"
+#     ]
+#   }
+# }
